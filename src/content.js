f88ace179bbaee52d2b0295a07ee4c857dd4ac53
Alexander Hjelm <alexander.hjelm@solidify.dev>
2023-04-16 16:21:04 +0200

    Lookup and search-replace in website contents

--- src/content.js
+++ src/content.js
@@ -1,7 +1,16 @@
 let divs = document.getElementsByTagName('div');
+
+wordsSearch = ["a", "e", "i"];
+wordsReplace = ["o", "o", "o"];
+
 for (var i = 0; i < divs.length; i++) {
     let div = divs[i];
     let resultText = div.innerHTML;
-    resultText = resultText.replaceAll("a", "b");
+    for (var j = 0; j < wordsSearch.length; j++) {
+        const wordSearch = wordsSearch[j];
+        const wordReplace = wordsReplace[j];
+        const regex = new RegExp("(?<!\\/\\/[\\S]+)"+wordSearch, 'gi');
+        resultText = resultText.replace(regex, wordReplace);
+    }
     div.innerHTML = resultText;
 }
